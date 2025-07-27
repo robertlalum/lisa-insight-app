81c04c287d7a24f9c45f12320d8772b88de8ec8c
robertlalum <73671123+robertlalum@users.noreply.github.com>
2025-07-26 19:59:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -38,10 +38,10 @@ def analyze():
         "suggestions": suggestions
     })
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
 @app.route('/')
 def home():
     return render_template('index.html')
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
