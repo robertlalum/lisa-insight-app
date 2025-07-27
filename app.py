d6d81bd9094014746a9d11da2e87cf4c3ddc7252
robertlalum <73671123+robertlalum@users.noreply.github.com>
2025-07-26 19:56:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
@@ -40,4 +40,8 @@ def analyze():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
